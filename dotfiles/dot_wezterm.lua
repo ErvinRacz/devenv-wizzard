cab6db6d65ea4d797411cc0d52f339de562621d8
Ervin Rácz <ervin.racz@protonmail.com>
2024-11-30 20:17:48 +0200

    Update .chezmoiignore Add dotfiles/.chezmoiscripts/pwsh-setup.ps1 Remove dotfiles/.chezmoiscripts/pwsh_setup.ps1 Add dotfiles/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/run_once_and-on-startup.ps1 Update dotfiles/.config/starship.toml Update dotfiles/.wezterm.lua Update winget/packages.dsc.yaml

--- dotfiles/dot_wezterm.lua
+++ dotfiles/dot_wezterm.lua
@@ -8,9 +8,16 @@ local config = wezterm.config_builder()
 config.default_prog = { 'pwsh.exe', '-NoLogo' }
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox Red'
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
 config.window_background_opacity = 1
+-- config.window_background_opacity = 0
+-- config.win32_system_backdrop = 'Tabbed'
+-- config.front_end = "OpenGL" -- needed for backdrop
+
+config.initial_rows = 40
+config.initial_cols = 140
+
 
 -- and finally, return the configuration to wezterm
 return config
