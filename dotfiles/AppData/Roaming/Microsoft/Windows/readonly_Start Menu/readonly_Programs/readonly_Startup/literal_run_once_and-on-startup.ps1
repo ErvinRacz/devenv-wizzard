97a0e47e6884e445d09660d78abb0aea7df758dc
Ervin Rácz <raczervin@microsoft.com>
2024-11-30 20:57:52 +0200

    move startup script

--- dotfiles/AppData/Roaming/Microsoft/Windows/readonly_Start Menu/readonly_Programs/readonly_Startup/literal_run_once_and-on-startup.ps1
+++ dotfiles/AppData/Roaming/Microsoft/Windows/readonly_Start Menu/readonly_Programs/readonly_Startup/literal_run_once_and-on-startup.ps1
@@ -1,22 +1,22 @@
-$chezmoiSourcePath = & chezmoi source-path
+# $chezmoiSourcePath = & chezmoi source-path
 
-# Function to start and configure watchman
-function Start-WatchmanForChezmoi {
-    # Start watching the chezmoi source path
-    Write-Host "Setting up watchman for path: $chezmoiSourcePath"
-    & watchman watch $chezmoiSourcePath
+# # Function to start and configure watchman
+# function Start-WatchmanForChezmoi {
+#     # Start watching the chezmoi source path
+#     Write-Host "Setting up watchman for path: $chezmoiSourcePath"
+#     & watchman watch $chezmoiSourcePath
 
-    # Define the trigger for 'chezmoi apply --force'
-    $triggerConfig = @"
-[
-    "trigger", "$chezmoiSourcePath", {
-        "name": "chezmoi-apply",
-        "command": ["chezmoi", "apply", "--force"]
-    }
-]
-"@
-    Write-Host "Configuring watchman trigger for chezmoi apply..."
-    & watchman -j $triggerConfig
-}
+#     # Define the trigger for 'chezmoi apply --force'
+#     $triggerConfig = @"
+# [
+#     "trigger", "$chezmoiSourcePath", {
+#         "name": "chezmoi-apply",
+#         "command": ["chezmoi", "apply", "--force"]
+#     }
+# ]
+# "@
+#     Write-Host "Configuring watchman trigger for chezmoi apply..."
+#     & watchman -j $triggerConfig
+# }
 
-Start-WatchmanForChezmoi
+# Start-WatchmanForChezmoi
--- dotfiles/dot_wezterm.lua
+++ dotfiles/dot_wezterm.lua
@@ -10,7 +10,7 @@ config.default_prog = { 'pwsh.exe', '-NoLogo' }
 -- For example, changing the color scheme:
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
-config.window_background_opacity = 1
+config.window_background_opacity = 0.1
 -- config.window_background_opacity = 0
 -- config.win32_system_backdrop = 'Tabbed'
 -- config.front_end = "OpenGL" -- needed for backdrop
