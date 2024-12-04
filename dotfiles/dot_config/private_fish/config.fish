fa44d1bfc6b950bbbb56e6c0cd1810d018abc9af
Ervin Rácz <ervin.racz@protonmail.com>
2024-12-04 13:18:20 +0200

    apply changes

--- dotfiles/dot_config/private_fish/config.fish
+++ dotfiles/dot_config/private_fish/config.fish
@@ -1,14 +1,11 @@
 if status is-interactive
-	# Commands to run in interactive sessions can go here
-	starship init fish | source
 end
 
-function fish_user_key_bindings
-	fish_vi_key_bindings
-end
-
+fish_vi_key_bindings
+starship init fish | source
 set fish_cursor_visual block
-bind \cm 'echo "test"'
-
-bind \ci nextd
-
+set -a PATH /opt/nvim-linux64/bin
+set -gx EDITOR nvim
+bind --erase \cN
+bind --mode insert \cN 'nvim .'
+bind --mode command \cN 'nvim .'
